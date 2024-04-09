efcd60f4c816dcff0a2d3d29d2f2dbf42d632abb
Resmini Loris <resminiloris@gmail.com>
2024-04-09 12:31:54 +0200

    implemented news api

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import requests
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+url = (
+    "https://newsapi.org/v2/everything?q=tesla&from=2024-03-09&sortBy=publishedAt&" 
+    "apiKey=da82b67ed137451ab20fe574f738a71e"
+)
 
+request = requests.get(url)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+content = request.text
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(content)
